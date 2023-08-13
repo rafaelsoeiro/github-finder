ce89a762d70f2e4e87bd0249494a8b4c8aa4e905
rafaelsoeiro <rafaelssoeiro@gmail.com>
2023-08-13 17:08:27 -0300

    :sparkles: feat: create home component

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import { useState } from "react";
+import { Search } from "../components/Search";
+import { UserProps } from "../types/user";
 
 export const Home = () => {
+  const [user, setUser] = useState<UserProps | null>(null);
+
+  const loadUser = async (username: string) => {
+    const res = await fetch(`https://api.github.com/users/${username}`);
+    const data = await res.json();
+
+    const { avatar_url, login, location, followers, following } = data;
+
+    const userData: UserProps = {
+      avatar_url,
+      login,
+      location,
+      followers,
+      following,
+    };
+
+    setUser(userData);
+  };
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <h2>Home</h2>
+      <Search loadUser={loadUser} />
+      {user && <p>{user.login}</p>}
+    </div>
+  );
+};
