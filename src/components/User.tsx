b6ad808efc5f13d69bc8f489f5dab7b90cae1eea
rafaelsoeiro <rafaelssoeiro@gmail.com>
2023-09-15 02:50:44 -0300

    :lipstick: feat: create css for user component and style the component

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -2,6 +2,8 @@ import { Link } from "react-router-dom";
 import { UserProps } from "../types/user";
 import { MdLocationPin } from "react-icons/md";
 
+import classes from "./User.module.css";
+
 const User = ({
   login,
   avatar_url,
@@ -10,23 +12,23 @@ const User = ({
   location,
 }: UserProps) => {
   return (
-    <div>
+    <div className={classes.user}>
       <img src={avatar_url} alt={login} />
       <h2>{login}</h2>
       {location && (
-        <p>
-          {MdLocationPin}
+        <p className={classes.location}>
+          <MdLocationPin/>
           <span>{location}</span>
         </p>
       )}
-      <div>
+      <div className={classes.stats}>
         <div>
           <p>Seguidores: </p>
-          <p>{followers}</p>
+          <p className={classes.number}>{followers}</p>
         </div>
         <div>
           <p>Seguindo: </p>
-          <p>{following}</p>
+          <p className={classes.number}>{following}</p>
         </div>
       </div>
 
